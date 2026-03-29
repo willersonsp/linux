16a29a324493c39db556906c02619c90873d9aeb
OpenIPC <dev@openipc.org>
2026-03-29 13:24:30 +0300

    Add DT_MACHINE_START for hi3516ev300 (was missing, caused dump_machine_table hang)

--- arch/arm/mach-hibvt/mach-hi3516ev300.c
+++ arch/arm/mach-hibvt/mach-hi3516ev300.c
@@ -17,9 +17,48 @@
 */
 
 #include <linux/of_address.h>
+#include <linux/clocksource.h>
+#include <linux/irqchip.h>
+
 #include <asm/smp_scu.h>
+#include <asm/mach/arch.h>
+#include <asm/mach/map.h>
+#include <mach/io.h>
 
 #include "mach-common.h"
+
+static struct map_desc hi3516ev300_io_desc[] __initdata = {
+    {
+        .virtual    = HI3518EV20X_IOCH1_VIRT,
+        .pfn        = __phys_to_pfn(HI3518EV20X_IOCH1_PHYS),
+        .length     = HI3518EV20X_IOCH1_SIZE,
+        .type       = MT_DEVICE
+    },
+    {
+        .virtual    = HI3518EV20X_IOCH2_VIRT,
+        .pfn        = __phys_to_pfn(HI3518EV20X_IOCH2_PHYS),
+        .length     = HI3518EV20X_IOCH2_SIZE,
+        .type       = MT_DEVICE
+    },
+};
+
+static void __init hi3516ev300_map_io(void)
+{
+    iotable_init(hi3516ev300_io_desc, ARRAY_SIZE(hi3516ev300_io_desc));
+}
+
+static const char *const hi3516ev300_compat[] __initconst = {
+    "hisilicon,hi3516ev200",
+    "hisilicon,hi3516ev300",
+    "hisilicon,hi3518ev300",
+    "hisilicon,hi3516dv200",
+    NULL,
+};
+
+DT_MACHINE_START(HI3516EV300_DT, "Hisilicon HI3516EV300 (Flattened Device Tree)")
+    .map_io     = hi3516ev300_map_io,
+    .dt_compat  = hi3516ev300_compat,
+MACHINE_END
 
 #ifdef CONFIG_SMP
 
